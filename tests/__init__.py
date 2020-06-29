680bba7f54635a4796c96a6f6ed879a43f97f7ea
rbuerki <r2d4@bluewin.ch>
2020-06-29 17:02:29 +0200

    Style formatting

--- tests/__init__.py
+++ tests/__init__.py
@@ -5,7 +5,7 @@ def assert_df_equals(df1, df2):
     assert df1.columns == df2.columns
     for values1, values2 in zip(df1._data.values(), df2._data.values()):
         kind = values1.dtype.kind
-        if kind == 'f':
+        if kind == "f":
             assert_allclose(values1, values2)
         else:
             assert_array_equal(values1, values2)
